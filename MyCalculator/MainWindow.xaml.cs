bb1f14412e6eeb0c54c0ac9d2f519b8b0cd55327
Justin <justhaze08@gmail.com>
2016-11-30 23:51:40 -0500

    want to come back and do level 3 or 4

--- MyCalculator/MainWindow.xaml.cs
+++ MyCalculator/MainWindow.xaml.cs
@@ -22,11 +22,21 @@ namespace Calculator
     {
         public double mpresult { get; set; } 
         public double mrresult { get; set; }
+        public double mmresult { get; set; }
+        public string Textbox { get; } = "0";
+
         public MainWindow()
         {
             InitializeComponent();
+           
         }
+        //trying to clear the 0 that is there already but it wont clear on a click event
+        private void input1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            input1.Text = String.Empty;
 
+        }
+        //When you click the addition button, it will add the two entered values together
         private void Addition_Click(object sender, RoutedEventArgs e)
         {
 
@@ -35,7 +45,7 @@ namespace Calculator
             double result = (A + B);
             this.Result.Content = result;
         }
-
+        //When you click the subtraction button, it will subtract the two entered values together
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             double A = Convert.ToDouble(this.input1.Text);
@@ -43,7 +53,7 @@ namespace Calculator
             double result = (A - B);
             this.Result.Content = result;
         }
-
+        //When you click the multiplication button, it will multiply the two entered values together
         private void button2_Click(object sender, RoutedEventArgs e)
         {
             double A = Convert.ToDouble(this.input1.Text);
@@ -51,7 +61,7 @@ namespace Calculator
             double result = (A * B);
             this.Result.Content = result;
         }
-
+        //When you click the division button, it will divide the two entered values together
         private void button3_Click(object sender, RoutedEventArgs e)
         {
             double A = Convert.ToDouble(this.input1.Text);
@@ -59,7 +69,7 @@ namespace Calculator
             double result = (A / B);
             this.Result.Content = result;
         }
-
+        //When you click the modulus button, it will return the absolute value of the two entered values together
         private void button4_Click(object sender, RoutedEventArgs e)
         {
             double A = Convert.ToDouble(this.input1.Text);
@@ -68,14 +78,24 @@ namespace Calculator
             this.Result.Content = result;
         }
 
+        //was not able to get my level 2 memory buttons to work
         private void button1_Click_1(object sender, RoutedEventArgs e)
         {
-            this.Result.Content = mpresult;
+            
+        }
+        private void button2_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void button3_Click_1(object sender, RoutedEventArgs e)
         {
-            this.Result.Content = mrresult;
+            
+        }
+
+        private void button4_Click_1(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
